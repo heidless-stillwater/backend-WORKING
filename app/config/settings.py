79c00c263feb4d26ed815a4414af2fabb9bc1af5
heidless <rob.lockhart@yahoo.co.uk>
2023-02-22 11:21:10 +0000

    NOT WORKING - YET!!!

--- app/config/settings.py
+++ app/config/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+ 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
